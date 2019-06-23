cf2dc2d2301dfdb732cfd3737a824a102aa59567
Madongming <elsove812@gmail.com>
2019-06-23 15:54:47 +0800

    使用内置方法 	修改：     string-segmentation.go

--- algorithm/backtrack/string-segmentation.go
+++ algorithm/backtrack/string-segmentation.go
@@ -8,14 +8,6 @@ func partition(str string) [][]string {
 	return partitionRec(str, 0, nil, nil)
 }
 
-func deepCopySlice(sl []string) []string {
-	ret := make([]string, len(sl))
-	for i := range sl {
-		ret[i] = sl[i]
-	}
-	return ret
-}
-
 func partitionRec(str string, start int, result [][]string, elem []string) [][]string {
 	if start >= len(str) {
 		return append(result, elem)
@@ -24,7 +16,9 @@ func partitionRec(str string, start int, result [][]string, elem []string) [][]s
 	for end := start; end < len(str); end++ {
 		elem = append(elem, str[start:end+1])
 		result = partitionRec(str, end+1, result, elem)
-		elem = deepCopySlice(elem[0 : len(elem)-1])
+		tmp := make([]string, len(elem)-1)
+		copy(tmp, elem[0:len(elem)-1])
+		elem = tmp
 	}
 	return result
 }
